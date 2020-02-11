1c9b226c0fe8b36ad5aa57dcc50142e232db729e
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-11 21:18:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ holiday_hash.each do |season, holiday|
  puts "#{season.capitalize.to_s}:"
  holiday.each do |day, supplies|
 
-   puts "  #{day.to_s.split("_").capitalize.join(" ")}: #{supplies.join(", ")}"
+   puts "  #{day.split("_").capitalize.join(" ")}: #{supplies.join(", ")}"
 
 
 
